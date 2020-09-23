9c9faf18ce6d3569244eb17ba8d33775f50a9a25
jasonpereira96 <jasonpereira074@gmail.com>
2020-09-23 12:31:38 +0530

    updating server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 5000;
 
 app.use('/', express.static(path.join(__dirname, 'build')));
+// app.get('/', (request, response) => response.render('pages/index'));
 
 app.listen(PORT, function () {
     console.log('Serving frontend on port: ' + PORT);
